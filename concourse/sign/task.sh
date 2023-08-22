7bee68b1e8492fa7b520bf8ce156692a41a29e79
David Timm <dtimm@vmware.com>
2023-08-22 12:25:27 -0500

    unique signed bundles
    Co-authored-by: Ryan Hall <hallr@vmware.com>

--- concourse/sign/task.sh
+++ concourse/sign/task.sh
@@ -5,5 +5,5 @@ echo "${SIGNING_KEY:?SIGNING_KEY must be set}"
 echo ${SIGNING_KEY} > /tmp/cosign.key
 
 for FILE in sboms/*; do
-  cosign sign-blob --yes --tlog-upload=false --key /tmp/cosign.key --bundle attestations/cosign.bundle ${FILE}
+  cosign sign-blob --yes --tlog-upload=false --key /tmp/cosign.key --bundle attestations/${FILE#sboms/}.cosign.bundle ${FILE}
 done
