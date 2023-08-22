fc1abeaef8d2a11df216cf94c3f82adad8d1b463
David Timm <dtimm@vmware.com>
2023-08-22 11:49:18 -0500

    generate sboms for all gomods
    Co-authored-by: Ryan Hall <hallr@vmware.com>

--- concourse/gomod/task.sh
+++ concourse/gomod/task.sh
@@ -1,14 +1,46 @@
 #!/usr/bin/env bash
 set -eux
 
-echo "${GO_MOD_FILE:?GO_MOD_FILE must be set}"
+echo "${PACKAGE_NAME:?PACKAGE_NAME must be set}"
 sbom_format="${SBOM_FORMAT:-cyclonedx-json}"
 
-echo "Generating SBoM for ${GO_MOD_FILE}"
+spec_file="bosh-release/packages/$PACKAGE_NAME/spec"
 
-package_sbom="$(syft packages "file:${GO_MOD_FILE}" --output "${sbom_format}")"
-echo "Metadata for SBoM:"
-echo "${package_sbom}" | jq .metadata
+# Check if spec file exists
+if [[ ! -f "$spec_file" ]]; then
+  echo "spec file not found for $PACKAGE_NAME. Skipping..."
+  return
+fi
 
-jsonString=$(echo "${package_sbom}" | jq --join-output --compact-output .)
-echo "${jsonString}" > sbom/sbom.json
+echo "Finding go.mod files for $PACKAGE_NAME..."
+
+gomods=()
+while IFS= read -r line
+do
+  # Check if spec file contains go.mod
+  if [[ "$line" =~ "go.mod" ]]; then
+    # Extract relative path from line
+    rel_path=$(echo $line | cut -d':' -f2 | tr -d ' ' | tr -d '"')
+    rel_path=${rel_path#"-"}
+
+    # Absolute path to the go.mod file
+    abs_path="bosh-release/src/$rel_path"
+
+    # If go.mod exists, add it to the list
+    for f in $(ls $abs_path); do
+      echo "go.mod found in $f"
+      gomods+=("$f")
+    done
+  fi
+done < "$spec_file"
+
+uniq_gomods=($(for g in "${gomods[@]}"; do echo "${g}"; done | sort -u))
+for gomod in "${uniq_gomods[@]}"; do
+  echo "Generating SBoM for ${gomod}"
+  package_sbom="$(syft packages "file:${gomod}" --output "${sbom_format}")"
+
+  jsonString=$(echo "${package_sbom}" | jq --join-output --compact-output .)
+
+  uuid=$(uuidgen)
+  echo "${jsonString}" > "sboms/${uuid}.sbom.json"
+done
