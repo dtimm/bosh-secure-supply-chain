d58041567445032e0955d90af43bef8e6b6121ba
David Timm <dtimm@vmware.com>
2023-08-24 09:31:14 -0500

    add go details to attestations

--- gomod_vendor/callback_action/generate_layout.sh
+++ gomod_vendor/callback_action/generate_layout.sh
@@ -4,15 +4,19 @@ set -euo pipefail
 echo "${GO_MOD_FILE:?GO_MOD_FILE must be set}"
 go_sum_file="$(dirname "${GO_MOD_FILE}")/go.sum"
 
+go_bin="$(which go)"
+go_hash=$(sha256sum "${go_bin}" | awk '{print $1}')
+go_version="$(go version)"
+
 mod_hash=$(sha256sum "${GO_MOD_FILE}" | awk '{print $1}')
 printf -v gomod_subject \
-    '{"name": "%s", "digest": {"sha256": "%s"}}' \
-    "${GO_MOD_FILE}" "$mod_hash"
+    '{"name": "%s", "digest": {"sha256": "%s"}, "actions": ["go mod tidy", "go mod vendor"], "tools": [{"name": "%s", "digest": {"sha256": "%s"}, "version": "%s"}]}' \
+    "${GO_MOD_FILE}" "${mod_hash}" "${go_bin}" "${go_hash}" "${go_version}"
 
 sum_hash=$(sha256sum "${go_sum_file}" | awk '{print $1}')
 printf -v gosum_subject \
-    '{"name": "%s", "digest": {"sha256": "%s"}}' \
-    "${go_sum_file}" "$sum_hash"
+    '{"name": "%s", "digest": {"sha256": "%s"}, "actions": ["go mod tidy", "go mod vendor"], "tools": [{"name": "%s", "digest": {"sha256": "%s"}, "version": "%s"}]}' \
+    "${go_sum_file}" "${sum_hash}" "${go_bin}" "${go_hash}" "${go_version}"
 
 cat <<EOF >DATA
 {
