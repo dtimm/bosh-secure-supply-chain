24428a0a56e11e7a00f354026afb5f8297a546d9
David Timm <dtimm@vmware.com>
2023-08-31 14:56:05 -0500

    remove sign task

--- concourse/sign/task.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-echo "${SIGNING_KEY:?SIGNING_KEY must be set}"
-echo -e ${SIGNING_KEY} > /tmp/cosign.key
-
-for FILE in sboms/*; do
-  cosign sign-blob --yes --tlog-upload=false --key /tmp/cosign.key --bundle attestations/${FILE#sboms/}.cosign.bundle ${FILE}
-done
